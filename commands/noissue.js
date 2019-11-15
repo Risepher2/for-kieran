83d2f9ec0a0470eddd104ff17c078c7c4d8c04f9
Archie <54244039+variakle@users.noreply.github.com>
2019-11-15 21:08:41 +0000

    Delete noissue.js

--- commands/noissue.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-
-    message.channel.send('**No known issues as of now, report any issues you encounter.**')
-    
-};
-
-exports.help = {
-    name: 'noissue'
-};
